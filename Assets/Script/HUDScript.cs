71eb3d073db740f24cc3128386c5b6f759dcdccc
felixgiov <gv.felix@yahoo.co.id>
2014-12-20 09:45:32 +0700

    ln

---
+++ Assets/Script/HUDScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	float playerScore = 0;
+
+	void Update () {
+		playerScore += Time.deltaTime;
+	}
+
+	public void IncreaseScore(int amount)
+	{
+		playerScore += amount;
+	}
+
+	void OnDisable()
+	{
+		PlayerPrefs.SetInt ("Score", (int)(playerScore * 100));
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore * 100));
+	}
+}
---
+++ Assets/Script/PowerupScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour {
+
+	HUDScript hud;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
+			hud.IncreaseScore(10);
+			Destroy (this.gameObject);
+		}
+	}
+
+}
